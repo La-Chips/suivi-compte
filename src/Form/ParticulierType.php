168224209f06c814a961d775336cc00b02886afb
TTemps <fuller10212@gmail.com>
2022-04-28 18:47:15 +0200

    template particulier  + catégorie mis à jour

--- src/Form/ParticulierType.php
+++ src/Form/ParticulierType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Particulier;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,15 +13,60 @@ class ParticulierType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('mail')
-            ->add('rue')
-            ->add('cp')
-            ->add('ville')
-            ->add('pays')
-            ->add('telephone')
-            ->add('civilite')
-            ->add('nom')
-            ->add('prenom')
+            ->add('mail', EmailType::class, [
+                'label' => 'Adresse mail',
+                'attr' => [
+                    'placeholder' => 'Adresse mail',
+                ],
+            ])
+            ->add('rue', null, [
+                'label' => 'Rue',
+                'attr' => [
+                    'placeholder' => 'Rue',
+                ],
+            ])
+            ->add('cp', null, [
+                'label' => 'Code postal',
+                'attr' => [
+                    'placeholder' => 'Code postal',
+                ],
+            ])
+            ->add('ville', null, [
+                'label' => 'Ville',
+                'attr' => [
+                    'placeholder' => 'Ville',
+                ],
+            ])
+            ->add('pays', null, [
+                'label' => 'Pays',
+                'attr' => [
+                    'placeholder' => 'Pays',
+                ],
+            ])
+            ->add('telephone', null, [
+                'label' => 'Téléphone',
+                'attr' => [
+                    'placeholder' => 'Téléphone',
+                ],
+            ])
+            ->add('civilite', null, [
+                'label' => 'Civilité',
+                'attr' => [
+                    'placeholder' => 'Civilité',
+                ],
+            ])
+            ->add('nom', null, [
+                'label' => 'Nom',
+                'attr' => [
+                    'placeholder' => 'Nom',
+                ],
+            ])
+            ->add('prenom', null, [
+                'label' => 'Prénom',
+                'attr' => [
+                    'placeholder' => 'Prénom',
+                ],
+            ])
         ;
     }
 
