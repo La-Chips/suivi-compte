ac2f470ceef515faa1484e8db8bd680a0c3001ab
TTemps <fuller10212@gmail.com>
2022-06-09 11:31:52 +0200

    wtf je n'avais pas tout push

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -119,6 +119,7 @@ class HomeController extends AbstractController
         }
         $lignes = $ligneRepository->findByMonth($year, $month, $sort, $order,$this->getUser());
         $sum = $ligneRepository->sumByMonth($month,$year,$this->getUser());
+
         return $this->render('home/see.html.twig', [
             'lignes' => $lignes,
             'sort' => $sort,
